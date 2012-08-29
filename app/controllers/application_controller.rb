55f4e42f883e51da6ac81d6ce28c31b1ed791a01
Daniel Harris <daniel.leonard.harris@aviva.co.uk>
2012-08-29 10:30:29 +0100

    Basic landing page and FAQ structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :mobile_device?
+  before_filter :prepare_for_mobile
+  
+  
+  private
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS|Android/
+  end
+  
+  def prepare_for_mobile
+     request.format = :mobile if mobile_device? 
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824092128) do
+ActiveRecord::Schema.define(:version => 20120828101118) do
+
+  create_table "faqs", :force => true do |t|
+    t.string   "question"
+    t.text     "answer"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "policies", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "students", :force => true do |t|
     t.string   "name"
